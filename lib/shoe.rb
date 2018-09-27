53291f767ef9bd582bfe68037dd9ad9b10c9f754
Harry Bloch <harryfbloch@gmail.com>
2018-09-27 22:46:17 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,8 +1,12 @@
 # Make your shoe class here!
 class Shoe
-  attr_accessor :brand
+  attr_accessor :brand, :color, :size, :material, :condition
   
   def initialize(brand)
     self.brand = brand
+  end
+  
+  def cobble
+    
   end
 end
